c7297913ad9de6f0bc2d176774e8d92bfa0ae003
Mitch T <mitchelltaylor@gmail.com>
2017-02-01 07:26:35 -0500

    fixing some problems, causing others

--- server.R
+++ server.R
@@ -1,68 +1,71 @@
 ### BODY COMPOSITION APP
 
-library(shiny)
+library(shiny); library(TTR)
 
-# Create dummy data
-date <- seq(as.Date("2017/1/1"), as.Date("2017/1/10"), "days")
-weight <- 180:171
-bf <- seq(23, 22.1, -0.1)
-sm <- seq(37, 37.9, 0.1)
-data <- data.frame(date = as.Date(date, origin="1970-01-01"), weight, bf, sm)
+setwd("C:/Users/MT84249/Desktop/personal/coursera/data_products/body_composition")
+data <- read.csv("weight_data.csv")
+data$date <- as.Date(data$date, format = "%m/%d/%Y")
 
 # Define server logic
 shinyServer(function(input, output) {
     values <- reactiveValues(df_data = data)
+    values <- reactiveValues(df_data_analysis = data)
     
     observeEvent(input$submit, {
         tmp <- data.frame(date = as.Date(input$date, origin="1970-01-01"), 
-                          weight = input$weight, bf = input$bf, sm = input$sm)
+                          weight = input$weight, bf = input$bf, sm = input$sm,
+                          vf = input$vf)
         values$df_data <- rbind(values$df_data, tmp)
+        values$df_data_analysis <- values$df_data
+        calculate()
     })    
     
     calculate <- reactive({
-        values$df_data$bodycomp <- values$df_data$sm / values$df_data$bf
-        values$df_data$weight_7ma <- SMA(values$df_data$weight, 7)
-        values$df_data$bf_7ma <- SMA(values$df_data$bf, 7)
-        values$df_data$sm_7ma <- SMA(values$df_data$sm, 7)
-        values$df_data$bc_7ma <- SMA(values$df_data$bodycomp, 7)
+        values$df_data_analysis$bodycomp <- values$df_data_analysis$sm / values$df_data_analysis$bf
+        values$df_data_analysis$weight_7ma <- SMA(values$df_data_analysis$weight, 7)
+        values$df_data_analysis$bf_7ma <- SMA(values$df_data_analysis$bf, 7)
+        values$df_data_analysis$sm_7ma <- SMA(values$df_data_analysis$sm, 7)
+        values$df_data_analysis$bc_7ma <- SMA(values$df_data_analysis$bodycomp, 7)
     })
     
     # view table
     output$datatable = renderDataTable({
-        values$df_data
+        calculate()
+        values$df_data_analysis
     })
     
     # weight plot
     output$weightplot = renderPlotly({
         calculate()
-        plot_ly(values$df_data, x = ~date, y = ~weight, 
+        plot_ly(values$df_data_analysis, x = date, y = weight, 
                 type = "scatter", mode = "lines") %>%
-        add_trace(y = ~weight_7ma, name = 'MA7', mode = 'lines')
+        add_trace(x = date, y = weight_7ma, name = 'MA7', mode = 'lines')
     })
     
     # bodyfat plot
     output$bfplot = renderPlotly({
         calculate()
-        plot_ly(values$df_data, x = ~date, y = ~bf, 
+        plot_ly(values$df_data_analysis, x = date, y = bf, 
                 type = "scatter", mode = "lines") %>%
-            add_trace(y = ~bf_7ma, name = 'MA7', mode = 'lines')
+            add_trace(x = date, y = bf_7ma, name = 'MA7', mode = 'lines')
     })
     
     # skeletal muscle plot
     output$smplot = renderPlotly({
         calculate()
-        plot_ly(values$df_data, x = ~date, y = ~sm, 
+        plot_ly(values$df_data_analysis, x = date, y = sm, 
                 type = "scatter", mode = "lines") %>%
-            add_trace(y = ~sm_7ma, name = 'MA7', mode = 'lines')
+            add_trace(x = date, y = sm_7ma, name = 'MA7', mode = 'lines')
     })
     
     # bodycomp plot
     output$bcplot = renderPlotly({
         calculate()
-        plot_ly(values$df_data, x = ~date, y = ~bodycomp, 
+        plot_ly(values$df_data_analysis, x = date, y = bodycomp, 
                 type = "scatter", mode = "lines") %>%
-            add_trace(y = ~bc_7ma, name = 'MA7', mode = 'lines')
+            add_trace(x = date, y = bc_7ma, name = 'MA7', mode = 'lines')
     })
     
 })
 
+class(data$date)
--- ui.R
+++ ui.R
@@ -13,19 +13,19 @@ shinyUI(fluidPage(
         sidebarPanel(
             dateInput("date", "Date:", Sys.Date()),
             numericInput("weight", "Weight:", 180),
-            numericInput("bf", "Body Fat %:", 23),
-            numericInput("sm", "Skeletal Muscle %:", 38),
-            #numericInput("othfat", "Other Fat Thing:", '')
+            numericInput("bf", "Body Fat %:", 0.23),
+            numericInput("sm", "Skeletal Muscle %:", 0.38),
+            numericInput("vf", "Visceral Fat:", 7),
             actionButton("submit", "Submit")
         ),
         # Plot Weight
         mainPanel(
             tabsetPanel(type = "tabs", 
-                        tabPanel("Data", dataTableOutput('datatable')), 
-                        tabPanel("Weight", plotlyOutput('weightplot')),
-                        tabPanel("Body Fat", plotlyOutput('bfplot')),
-                        tabPanel("Skeletal Muscle", plotlyOutput('smplot')),
-                        tabPanel("Body Composition", plotlyOutput('bcplot'))
+                        tabPanel("Data", dataTableOutput('datatable'))#, 
+                        #tabPanel("Weight", plotlyOutput('weightplot')),
+                        #tabPanel("Body Fat", plotlyOutput('bfplot')),
+                        #tabPanel("Skeletal Muscle", plotlyOutput('smplot')),
+                        #tabPanel("Body Composition", plotlyOutput('bcplot'))
             )
         )
     )
